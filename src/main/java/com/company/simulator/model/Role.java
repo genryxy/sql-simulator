3462a8e822dff1d108a1779cdb80a1638c6255b6
Alexandr <genryxy.alexandr@yandex.ru>
2021-01-08 02:42:39 +0300

    Add Admin role, add /user endpoint

--- src/main/java/com/company/simulator/model/Role.java
+++ src/main/java/com/company/simulator/model/Role.java
@@ -3,6 +3,7 @@ package com.company.simulator.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
+    ADMIN,
     USER;
 
     @Override
