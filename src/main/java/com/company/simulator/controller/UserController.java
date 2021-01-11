87bbec685849c47ac6ab156408ace11b0d7c435c
Alexandr <genryxy.alexandr@yandex.ru>
2021-01-12 01:59:02 +0300

    Add user profile

--- src/main/java/com/company/simulator/controller/UserController.java
+++ src/main/java/com/company/simulator/controller/UserController.java
@@ -2,13 +2,11 @@ package com.company.simulator.controller;
 
 import com.company.simulator.model.Role;
 import com.company.simulator.model.User;
-import com.company.simulator.repos.UserRepo;
-import java.util.Arrays;
+import com.company.simulator.service.UserService;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,18 +17,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         // Convert implicitly long to user by fetching from db
@@ -39,23 +38,31 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
         @RequestParam String username,
         @RequestParam Map<String, String> form,
         @RequestParam("userId") User user
     ) {
-        user.setUsername(username);
-        final Set<String> roles = Arrays.stream(Role.values())
-            .map(Role::name)
-            .collect(Collectors.toSet());
-        user.getRoles().clear();
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepo.save(user);
+        userService.saveUser(user, username, form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+        @AuthenticationPrincipal User user,
+        @RequestParam String password,
+        @RequestParam String email
+    ) {
+        userService.updateProfile(user, password, email);
+        return "redirect:/user/profile";
     }
 }
--- src/main/java/com/company/simulator/service/UserService.java
+++ src/main/java/com/company/simulator/service/UserService.java
@@ -3,8 +3,14 @@ package com.company.simulator.service;
 import com.company.simulator.model.Role;
 import com.company.simulator.model.User;
 import com.company.simulator.repos.UserRepo;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -39,6 +45,56 @@ public class UserService implements UserDetailsService {
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
+        sendMessage(user);
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        final User user = userRepo.findByActivationCode(code);
+        if (user == null) {
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepo.save(user);
+        return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        final Set<String> roles = Arrays.stream(Role.values())
+            .map(Role::name)
+            .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        final boolean isEmailChanged = !Objects.equals(email, user.getEmail());
+        if (isEmailChanged) {
+            user.setEmail(email);
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+        userRepo.save(user);
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
+    }
+
+    private void sendMessage(final User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
             final String message = String.format(
                 "Hello, %s!\nWelcome to SQL-simulator. Please, follow the link: %s%s",
@@ -48,16 +104,5 @@ public class UserService implements UserDetailsService {
             );
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-        return true;
-    }
-
-    public boolean activateUser(final String code) {
-        final User user = userRepo.findByActivationCode(code);
-        if (user == null) {
-            return false;
-        }
-        user.setActivationCode(null);
-        userRepo.save(user);
-        return true;
     }
 }
