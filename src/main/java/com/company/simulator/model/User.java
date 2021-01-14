d0a4379313e9cb42e1dead00292633e1c27acbda
EAVologzhanin <eavologzhanin@edu.hse.ru>
2021-01-14 21:34:34 +0300

    fix import

--- src/main/java/com/company/simulator/model/User.java
+++ src/main/java/com/company/simulator/model/User.java
@@ -1,15 +1,24 @@
 package com.company.simulator.model;
 
+import java.util.Collection;
+import java.util.Set;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.util.Collection;
-import java.util.Set;
 
 @Entity
 @Table(name = "person")
