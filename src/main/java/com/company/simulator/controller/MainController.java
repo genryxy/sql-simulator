6c51d158ab7ccd93ffe52a18ea32bc69e0c811eb
Alexander <38591972+genryxy@users.noreply.github.com>
2021-02-01 21:20:18 +0300

    View messages by user, add editor for them (#6)
    test squash 2 commits

--- src/main/java/com/company/simulator/controller/MainController.java
+++ src/main/java/com/company/simulator/controller/MainController.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,7 +23,8 @@ public final class MainController {
     private MessageRepo msgRepo;
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String greeting(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
         return "greeting";
     }
 
@@ -58,6 +61,42 @@ public final class MainController {
         final Iterable<Message> msgs = msgRepo.findAll();
         model.addAttribute("messages", msgs);
         return "main";
+    }
+
+    @GetMapping("/user-messages/{user}")
+    public String userMessages(
+        @AuthenticationPrincipal User currentUser,
+        @PathVariable User user,
+        Model model,
+        @RequestParam(required = false) Message message
+    ) {
+        model.addAttribute("messages", user.getMessages());
+        model.addAttribute("message", message);
+        model.addAttribute("isCurrentUser", currentUser.equals(user));
+        return "userMessages";
+    }
+
+    @PostMapping("/user-messages/{user}")
+    public String updateMessage(
+        @AuthenticationPrincipal User currentUser,
+        @PathVariable Long user,
+        @RequestParam(name = "id") Message message,
+        @RequestParam(name = "text") String text,
+        @RequestParam(name = "tag") String tag
+    ) {
+        // TODO: Return message in case of attempt
+        // to change someone else's message.
+        if (message.getAuthor().equals(currentUser)) {
+            if (!StringUtils.isEmpty(text)) {
+                message.setText(text);
+            }
+            if (!StringUtils.isEmpty(tag)) {
+                message.setTag(tag);
+            }
+            msgRepo.save(message);
+        }
+
+        return String.format("redirect:/user-messages/%d", user);
     }
 
 }
