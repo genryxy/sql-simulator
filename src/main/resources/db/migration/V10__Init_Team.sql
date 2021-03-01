7db6e6a20d3459aff4a5f56d9802789dae196702
Alexandr <genryxy.alexandr@yandex.ru>
2021-03-01 12:15:11 +0300

    #14 - Add constraint

--- src/main/resources/db/migration/V10__Init_Team.sql
+++ src/main/resources/db/migration/V10__Init_Team.sql
@@ -2,5 +2,6 @@ create table team (
     id         int8         not null,
     name       varchar(255) not null,
     invitation varchar(511) not null,
-    primary key (id)
+    primary key (id),
+    unique (invitation)
 );
