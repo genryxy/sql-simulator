61f2d917f304640785a0ad72054f53248f558dc7
EAVologzhanin <eavologzhanin@edu.hse.ru>
2021-03-02 16:17:44 +0300

    test commit

--- src/main/java/com/company/simulator/controller/teacher/TeacherTaskController.java
+++ src/main/java/com/company/simulator/controller/teacher/TeacherTaskController.java
@@ -1,10 +1,8 @@
-package com.company.simulator.controller;
+package com.company.simulator.controller.teacher;
 
-import com.company.simulator.transaction.SqlTransaction;
-import com.company.simulator.model.StudentQuery;
 import com.company.simulator.model.Task;
-import com.company.simulator.repos.StudentQueryRepo;
 import com.company.simulator.repos.TaskRepo;
+import com.company.simulator.transaction.SqlTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,18 +13,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-public class TaskController {
+@RequestMapping("/teacher")
+public class TeacherTaskController {
 
     @Autowired
     private TaskRepo taskRepo;
 
     @Autowired
     private SqlTransaction sqlTransaction;
+
+    @GetMapping("")
+    public String teacherPanel(Model model) {
+        return "teacher/panel";
+    }
 
     @GetMapping("/task")
     public String task(Model model) {
@@ -77,37 +81,5 @@ public class TaskController {
     public String addTask(@ModelAttribute Task task) {
         taskRepo.save(task);
         return ("task");
-    }
-
-    /*
-        From Alexander
-     */
-
-    @Autowired
-    private StudentQueryRepo queryRepo;
-
-    @GetMapping("practice/task/{task}")
-    public String taskById(
-            @PathVariable Task task,
-            Model model
-    ) {
-        model.addAttribute("task", task);
-        return "practice/taskExecution";
-    }
-
-    @PostMapping("practice/task/{task}")
-    public String saveStudentQuery(
-            @PathVariable Task task,
-            @RequestParam(name = "query") String query,
-            Model model
-    ) {
-        // TODO: Check student's answer. Is it correct query or wrong?
-        final StudentQuery stq = new StudentQuery();
-        stq.setCorrect(true);
-        stq.setQuery(query);
-        stq.setTask(task);
-        queryRepo.save(stq);
-        model.addAttribute("task", task);
-        return String.format("redirect:/practice/task/%d", task.getId());
     }
 }
