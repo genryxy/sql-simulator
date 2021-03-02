9b5e4e4522b71364b0f4bd4b86b1059661dc7cc0
EAVologzhanin <54524789+EAVologzhanin@users.noreply.github.com>
2021-03-02 14:59:43 +0300

    add transaction (#13)
    add transaction

--- src/main/java/com/company/simulator/controller/TaskController.java
+++ src/main/java/com/company/simulator/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.company.simulator.controller;
 
+import com.company.simulator.transaction.SqlTransaction;
 import com.company.simulator.model.StudentQuery;
 import com.company.simulator.model.Task;
 import com.company.simulator.repos.StudentQueryRepo;
@@ -16,17 +17,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 public class TaskController {
 
     @Autowired
     private TaskRepo taskRepo;
 
+    @Autowired
+    private SqlTransaction sqlTransaction;
+
     @GetMapping("/task")
     public String task(Model model) {
         return "task";
+    }
+
+    // TODO use this method in execute task by student
+    @GetMapping("/task/createTable")
+    public ResponseEntity executeQuery() {
+        String example ="create table test_table (\n" +
+                "    id      int8 not null,\n" +
+                "    tag     varchar(255),\n" +
+                "    text    varchar(65535) not null,\n" +
+                "    user_id int8,\n" +
+                "    primary key (id)\n" +
+                ");" +
+                "insert into test_table values (1,'123', '1234', 1);" +
+                "insert into test_table values (2,'123', '1234', 1);" +
+                "insert into test_table values (3,'123', '1234', 1);" +
+                "create table task2 (\n" +
+                "    id      int8 not null,\n" +
+                "    tag     varchar(255),\n" +
+                "    text    varchar(65535) not null,\n" +
+                "    user_id int8,\n" +
+                "    primary key (id)\n" +
+                ");";
+        return sqlTransaction.executeQuery(example, "select * from test_table", "select * from test_table");
     }
 
     @GetMapping("/task/all")
