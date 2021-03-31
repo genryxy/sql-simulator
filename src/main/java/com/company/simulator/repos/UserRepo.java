c80981094cc1cc73ec8520720754893e85686a86
EAVologzhanin <eavologzhanin@edu.hse.ru>
2021-03-31 23:27:05 +0300

    add removal

--- src/main/java/com/company/simulator/repos/UserRepo.java
+++ src/main/java/com/company/simulator/repos/UserRepo.java
@@ -2,20 +2,10 @@ package com.company.simulator.repos;
 
 import com.company.simulator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Collection;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 
     User findByActivationCode(String code);
-
-    @Query(
-        value = "SELECT * FROM user u " +
-            "JOIN practice_x_task p ON t.id=p.task_id " +
-            "WHERE t.category_id=?1 and p.practice_id=?2",
-        nativeQuery = true)
-    Collection<User> findAllTeam(Long teamId);
 }
