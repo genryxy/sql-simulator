2d211a32662674e2103f3a366b52c7d06dd9f6a9
EAVologzhanin <54524789+EAVologzhanin@users.noreply.github.com>
2021-04-03 19:30:01 +0300

    Add viewing and editing teams (#48)
    * add viewing and editing teams

--- src/main/java/com/company/simulator/repos/StudentRepo.java
+++ src/main/java/com/company/simulator/repos/StudentRepo.java
@@ -1,10 +1,17 @@
 package com.company.simulator.repos;
 
 import com.company.simulator.model.Student;
+import com.company.simulator.model.Team;
+import com.company.simulator.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepo extends CrudRepository<Student, Long> {
     Optional<List<Student>> findAllByUserId(Long user);
+
+    @Transactional
+    void deleteByUserAndTeam(User user, Team team);
 }
--- src/main/java/com/company/simulator/repos/TeamRepo.java
+++ src/main/java/com/company/simulator/repos/TeamRepo.java
@@ -13,29 +13,43 @@ import java.util.Optional;
 
 public interface TeamRepo extends CrudRepository<Team, Long> {
     Optional<Team> findTeamByInvitation(String invitation);
+
     Optional<List<Team>> findTeamsByPracticesContains(Practice practice);
+
     Optional<List<Team>> findTeamsByPracticesNotContainsAndAuthorId(Practice practice, Long id);
+
     Optional<List<Team>> findTeamsByAuthorId(Long authorId);
 
     @Query(
         value = "SELECT * FROM team t " +
-                    "JOIN student s on t.id = s.team_id " +
-                    "WHERE s.user_id = ?1",
+            "JOIN student s on t.id = s.team_id " +
+            "WHERE s.user_id = ?1",
         nativeQuery = true
     )
     Optional<List<Team>> findTeamsByStudentId(Long student);
 
     @Modifying
     @Query(
-            value = "INSERT INTO practice_x_team (practice_id, team_id) values (:practiceId, :teamId)",
-            nativeQuery = true)
+        value = "INSERT INTO practice_x_team (practice_id, team_id) values (:practiceId, :teamId)",
+        nativeQuery = true)
     @Transactional
     void assignPracticeToTeam(@Param("practiceId") Long practiceId, @Param("teamId") Long teamId);
 
     @Modifying
     @Query(
-            value = "DELETE FROM practice_x_team WHERE (practice_id = :practiceId AND team_id = :teamId)",
-            nativeQuery = true)
+        value = "DELETE FROM practice_x_team WHERE (practice_id = :practiceId AND team_id = :teamId)",
+        nativeQuery = true)
     @Transactional
     void throwPracticeToTeam(@Param("practiceId") Long practiceId, @Param("teamId") Long teamId);
+
+    @Modifying
+    @Query(
+        value = "update team set " +
+            "name = :name " +
+            "where id = :id",
+        nativeQuery = true)
+    @Transactional
+    void updateTeam(@Param("id") Long id,
+                    @Param("name") String name
+    );
 }
