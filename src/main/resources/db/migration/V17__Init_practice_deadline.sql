d4be2a56b4703e60ce52e6a428e37deb851ce28c
Alexandr <genryxy.alexandr@yandex.ru>
2021-04-12 22:15:57 +0300

    #79 - Edit script for existed practice, fix endpoint for admin

--- src/main/resources/db/migration/V17__Init_practice_deadline.sql
+++ src/main/resources/db/migration/V17__Init_practice_deadline.sql
@@ -5,6 +5,10 @@ create table practice_deadlines(
 	after_deadline boolean default false
 );
 
+insert into practice_deadlines (practice_id, date_start, date_end)
+ values (2, '2021-04-12 21:35:13.908944', '2022-10-21 21:35:13.908944');
+
+
 alter table if exists practice_deadlines
     add constraint practice_deadlines_practice_fk
         foreign key (practice_id) references practice
